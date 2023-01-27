70afe3c4f4dbde70eb0b955ccdc8e100a60e5c46
peaceOffiong <peaceyben@gamil.com>
2023-01-27 10:10:54 -0800

    Using props to pass data 1.1. complete

---
+++ part 1/reactexercise/src/components/Total.jsx
@@ -0,0 +1,9 @@
+const Total = ({exercises1, exercises2, exercises3}) => {
+  return (
+    <>
+      <p>Number of excercises {exercises1 + exercises2 + exercises3}</p>
+    </>
+  );
+}
+
+export default Total 
