0fef1c322323e2b7713c6f5058eb28d28bfffd19
peaceOffiong <peaceyben@gamil.com>
2023-02-01 03:08:36 -0800

    2.1 completed

---
+++ part 2/courseinfo/src/components/Total.js
@@ -0,0 +1,16 @@
+const Total = ({ parts }) => {
+  const totalSum = () => {
+    let total = 0;
+    parts.map((part) => {
+      total += part.exercises;
+    });
+    return total;
+  };
+  return (
+    <>
+      <p>{`Number of exercises ${totalSum()}`}</p>
+    </>
+  );
+};
+
+export default Total;
